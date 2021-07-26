cfbccc6cc64742ea9326cde230241a1ae19043f7
Mehwish-nedian <49073146+Mehwish-nedian@users.noreply.github.com>
2021-07-26 03:59:25 -0700

    Update params with watchers
    Update component according to dynamic path with router and with props

--- src/Vue router/components/teams/TeamMembers.vue
+++ src/Vue router/components/teams/TeamMembers.vue
@@ -9,6 +9,15 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to team 2</router-link>
+    <!--
+      Here we are trying to go to the same page with different id/parameter
+      This won't work until you add watcher to teamId 
+      The problem here is that vue can see that you are referring to the same page
+      so, it doesn't destroy your component and the same (previous) component continues to display on the screen
+      But if you notice carefully on the url, you will notice that the url is changed 
+      So, we add watcher to $router and updates the component accordingly.
+     -->
   </section>
 </template>
 
@@ -19,15 +28,74 @@ export default {
   components: {
     UserItem
   },
+  props:['teamId'],
+  inject: ['teams', 'users'],
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
+      teamName: '',
+      members: [],
     };
   },
+  methods:{
+    /*loadComponentviaRoute works perfectly fine but there is one problem here
+    Since we are using route to create the component. 
+    Now we cannot use it directly inside any other component like(<team-members></team-members)
+    The reusability of the component decreased,
+    To solve this we can use teamId to load the component instead of route and also add watcher to teamId otherwise it won't work.
+    */
+   /* loadComponentviaRoute(route){
+
+    const teamId = route.params.teamId; // Getting what's after /teams/ in url
+    // To select the team with that teamId
+    const selectedTeam = this.teams.find(team => team.id === teamId);
+
+    const selectedMembers = [];
+    // members array inside selected team object
+    const members = selectedTeam.members;
+    // Looping through members array inside selectedTeam object
+    for(const member of members){
+      // Find the users with the sam id as that of elements of member array
+      const selected = this.users.find(user =>user.id === member);
+      selectedMembers.push(selected)
+    }
+    this.teamName = selectedTeam.name;
+    this.members = selectedMembers;
+    },*/
+    loadComponentViaProp(teamId){
+       // To select the team with that teamId
+    const selectedTeam = this.teams.find(team => team.id === teamId);
+
+    const selectedMembers = [];
+    // members array inside selected team object
+    const members = selectedTeam.members;
+    // Looping through members array inside selectedTeam object
+    for(const member of members){
+      // Find the users with the sam id as that of elements of member array
+      const selected = this.users.find(user =>user.id === member);
+      selectedMembers.push(selected)
+    }
+    this.teamName = selectedTeam.name;
+    this.members = selectedMembers;
+    },
+  }
+  ,
+  created(){
+    /* Loading component via prop is a better option */
+    this.loadComponentViaProp(this.teamId)
+  },
+  watch:{
+    $route(newRoute){
+      /* Now router-link will start working properly */
+      //this.loadComponentviaRoute(newRoute);
+      console.log(newRoute);
+      
+
+    },
+    teamId(newId){
+      this.loadComponentViaProp(newId);
+    }
+  }
+
 };
 </script>
 
