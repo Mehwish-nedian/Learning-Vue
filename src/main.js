6dd08c0479633b9a8506eca6affc31e094bc2918
Mehwish-nedian <49073146+Mehwish-nedian@users.noreply.github.com>
2021-07-26 04:28:30 -0700

    Add router, handle notfound state, redirecting

--- src/main.js
+++ src/main.js
@@ -1,11 +1,48 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import {createRouter, createWebHistory} from 'vue-router';
 
-const app = createApp(App);
-import baseCard from './components/UI/BaseCard.vue';
-import baseButton from './components/UI/BaseButton.vue';
-import baseDialog from './components/UI/BaseDialog.vue';
-app.component('base-card',baseCard);
-app.component('base-button', baseButton);
-app.component("base-dialog", baseDialog);
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue';
+import usersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      redirect: "/teams"
+      // We can also add any separatr component here But that will not change url Redirect will...
+    },
+    {
+      path: "/teams",
+      component: TeamsList,
+      //alias: "/" // Usign alias will load this component on '/' without changing url
+    },
+    {
+      path: "/users",
+      component: usersList
+    },
+    {
+      // This object should always be placed after object with path of '/teams' or '/teams/something'
+      path: "/teams/:teamId",
+      //Any thing in the url after /teams/ will be treated ad teamID
+      // We can access what's stored in teamId by using (this.$route.params.teamId) in that component i.e TeamMembers
+      component: TeamMembers
+    },
+    {
+      //should always be placed at last 
+      path: '/:notFound(.*)', //notFound name is not keyword any name would work
+      //component: NotFound, To load error page if you have
+      redirect: '/users', // to redirect to other route
+    }
+  ]
+  //linkActiveClass: 'activelink',
+  // We can change link-active-class to any classname of our choice and style it usng this class
+  //linkExactActiveClass: 'exactactivelink' //Similar case as linkActiveClass
+});
+
+const app = createApp(App)
+app.use(router);
 app.mount('#app');
+
